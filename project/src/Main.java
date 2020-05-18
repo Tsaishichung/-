00bab73ced02d3fcd555aa3cab1496263d21827e
caizhichong <tshichung@gmail.com>
2020-05-19 01:04:00 +0800

    更新分享内容

--- project/src/Main.java
+++ project/src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
 
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Main main = new Main();
+        int x = 8,y = 6;
+        int res = main.add(x, y);
+        System.out.println(res);
+    }
+
+    public int add(int x, int y){
+        int sum = x + y;
+        return sum;
     }
 }
