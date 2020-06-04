e5893b41a4fffd61eaa7080dfff6e6d9f8cd7551
caizhichong <caizhichong@thunisoft.com>
2020-06-04 20:40:07 +0800

    leetcode简单题目

---
+++ project/src/leetcode/回文数/Main.java
@@ -0,0 +1,28 @@
+package leetcode.回文数;
+
+/**
+ * Main
+ *
+ * @author caizhichong
+ * @version V1.0.0
+ * @date 2020年06月04日 9:02
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(1234321));
+    }
+
+    private static boolean isPalindrome(int x){
+        if(x < 0){
+            return false;
+        }
+        int nb = x;
+        int n = 0;
+        while(x != 0){
+            n = n*10 + x%10;
+            x/=10;
+        }
+        return n==nb;
+    }
+}
---
+++ project/src/leetcode/整数反转/Main.java
@@ -0,0 +1,23 @@
+package leetcode.整数反转;
+
+/**
+ * Main
+ *
+ * @author caizhichong
+ * @version V1.0.0
+ * @date 2020年06月03日 17:32
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(reverse(-12345678));
+    }
+    private static int reverse(int x){
+        long n = 0;
+        while(x!=0){
+            n = n*10 + x%10;
+            x /=10;
+        }
+        return (int)n == n? (int)n : 0;
+    }
+}
