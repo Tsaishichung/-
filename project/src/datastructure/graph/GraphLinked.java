8b963a9491da97d2fb84e17f796489988e9654ae
caizhichong <caizhichong@thunisoft.com>
2020-11-09 20:18:00 +0800

    数据结构：图的邻接表以及邻接矩阵表示方式，图的深度优先与广度优先遍历

--- project/src/datastructure/graph/GraphLinked.java
+++ project/src/datastructure/graph/GraphLinked.java
@@ -1,5 +1,9 @@
 package datastructure.graph;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /**
  * GraphLinked 图-邻接表
  *
@@ -9,7 +13,315 @@ package datastructure.graph;
  */
 public class GraphLinked {
 
+    public static void main(String[] args) {
+        GraphA graph = buildGraph();
+        List<Integer> noInDegreeVertix = graph.getVertix();
+        boolean[] visited = new boolean[graph.getNodes().length];
+        for(Integer vertixIndex : noInDegreeVertix){
+            //深度优先遍历
+            //dfs(graph, vertixIndex, visited);
+            //广度优先遍历
+            bfs(graph, vertixIndex, visited);
+        }
+
+
+    }
+
+
+    /**
+     * GraphLinked
+     * @description 深度优先遍历
+     * @param graphA 图
+     * @param vertixIndex 当前顶点下标
+     * @param visited 顶点遍历情况
+     * @return
+     * @author caizhichong
+     * @date 2020/11/4
+     * @version V1.0
+     */
+    private static void dfs(GraphA graphA, Integer vertixIndex, boolean[] visited){
+        if(visited[vertixIndex]){
+            return;
+        }
+        visited[vertixIndex] = true;
+        System.out.println("节点：" + graphA.getNodes()[vertixIndex].getValue());
+        Vertix vertix = graphA.getNodes()[vertixIndex];
+        Node currentNode = vertix.getHead();
+        while(currentNode != null){
+            dfs(graphA, graphA.getVertixIndexByNodeValue(currentNode.getValue()), visited);
+            currentNode = currentNode.getNext();
+        }
+    }
+
+    /**
+     * GraphLinked
+     * @description 广度优先遍历
+     * @param graphA 图
+     * @param vertixIndex 当前顶点下标
+     * @param visited 顶点遍历情况
+     * @return
+     * @author caizhichong
+     * @date 2020/11/4
+     * @version V1.0
+     */
+    private static void bfs(GraphA graphA, Integer vertixIndex, boolean[] visited){
+        Queue<Vertix> queue = new LinkedList<>();
+        Vertix rootVertix = graphA.getNodes()[vertixIndex];
+        queue.add(rootVertix);
+        Vertix currentVertix;
+        while((currentVertix = queue.poll()) != null){
+            if(visited[graphA.getVertixIndexByNodeValue(currentVertix.getValue())]){
+                continue;
+            }
+            visited[graphA.getVertixIndexByNodeValue(currentVertix.getValue())] = true;
+            System.out.println("节点：" + currentVertix.getValue());
+            Node currentNode = currentVertix.getHead();
+            while(currentNode != null){
+                queue.add(graphA.getVertixByNodeValue(currentNode.getValue()));
+                currentNode = currentNode.getNext();
+            }
+        }
+    }
 
 
 
+
+    public static GraphA buildGraph(){
+        char[] vertix = new char[]{'a','g','c','f','e','d','b'};
+        GraphA graph = new GraphA(vertix);
+        graph.addEdge('a', 'b');
+        graph.addEdge('a', 'c');
+        graph.addEdge('b', 'c');
+        graph.addEdge('b', 'd');
+        graph.addEdge('c', 'd');
+        graph.addEdge('c', 'g');
+        graph.addEdge('d', 'e');
+        graph.addEdge('d', 'f');
+        graph.addEdge('e', 'f');
+        graph.addEdge('g', 'f');
+        /*char[] vertix = new char[]{'a','g','c','f','e','d','b','i','h','j'};
+        GraphA graph = new GraphA(vertix);
+        graph.addEdge('a', 'b');
+        graph.addEdge('a', 'c');
+        graph.addEdge('b', 'g');
+        graph.addEdge('g', 'f');
+        graph.addEdge('g', 'h');
+        graph.addEdge('g', 'i');
+        graph.addEdge('d', 'c');
+        graph.addEdge('d', 'e');
+        graph.addEdge('e', 'i');
+        graph.addEdge('c', 'g');
+        graph.addEdge('b', 'f');*/
+        return graph;
+    }
+
+}
+
+
+/**
+ * GraphLinked
+ * @description 图
+ * @author caizhichong
+ * @date 2020/11/3
+ * @version V1.0
+ */
+class GraphA{
+
+    /**
+     * 邻接表
+     * */
+    private Vertix[] nodes;
+
+    /**
+     * 逆邻接表
+     * */
+    private Vertix[] reverseNodes;
+
+    /**
+     * 节点下标位置,模拟最多只有英文字母26个节点
+     * */
+    private int[] nodeIndex = new int[26];
+
+    public GraphA(char[] nodes) {
+        super();
+        this.buildVertix(nodes);
+    }
+    private void buildVertix(char[] nodes){
+        this.nodes = new Vertix[nodes.length];
+        this.reverseNodes = new Vertix[nodes.length];
+        for(int i = 0; i < nodes.length; i++){
+            //存放顶点的下标
+            this.nodeIndex[this.getVertixIndex(nodes[i])] = i;
+            this.nodes[i] = new Vertix(nodes[i], null);
+            this.reverseNodes[i] = new Vertix(nodes[i], null);
+        }
+    }
+
+    public void addEdge(char from, char to){
+        this.buildVertix(from, to);
+    }
+
+    /**
+     * GraphA
+     * @description 通过值获取到节点
+     * @param ch 节点值
+     * @return
+     * @author caizhichong
+     * @date 2020/11/9
+     * @version V1.0
+     */
+    public Vertix getVertixByNodeValue(char ch){
+        return this.getNodes()[this.getVertixIndexByNodeValue(ch)];
+    }
+
+
+
+    /**
+     * GraphA
+     * @description 通过值获取到节点下标
+     * @param ch 节点值
+     * @return
+     * @author caizhichong
+     * @date 2020/11/9
+     * @version V1.0
+     */
+    public int getVertixIndexByNodeValue(char ch){
+        return this.getNodeIndex()[this.getVertixIndex(ch)];
+    }
+
+    /**
+     * GraphA
+     * @description 获取没有入度的节点
+     * @return
+     * @author caizhichong
+     * @date 2020/11/4
+     * @version V1.0
+     */
+    public List<Integer> getVertix(){
+        List<Integer> noInDegreeVertix = new LinkedList<>();
+        for(int i = 0; i < this.reverseNodes.length; i++){
+            if(this.reverseNodes[i].getHead() == null){
+                noInDegreeVertix.add(i);
+            }
+        }
+        return noInDegreeVertix;
+    }
+
+
+    private void buildVertix(char from, char to){
+        // need to validate parameter
+        Vertix fromVertix = this.nodes[this.nodeIndex[this.getVertixIndex(from)]];
+        Vertix toVertix = this.reverseNodes[this.nodeIndex[this.getVertixIndex(to)]];
+        //构建邻接表
+        Node fromHead = fromVertix.getHead();
+        Node fromNewHead = new Node(toVertix.getValue(),null);
+        fromNewHead.setNext(fromHead);
+        fromVertix.setHead(fromNewHead);
+        //构建逆邻接表
+        Node toHead = toVertix.getHead();
+        Node toNewHead = new Node(fromVertix.getValue(), null);
+        toNewHead.setNext(toHead);
+        toVertix.setHead(toNewHead);
+        this.nodes[this.nodeIndex[this.getVertixIndex(from)]] = fromVertix;
+        this.reverseNodes[this.nodeIndex[this.getVertixIndex(to)]] = toVertix;
+    }
+
+
+    public int getVertixIndex(char vertix){
+        return vertix - 'a';
+    }
+
+    public Vertix[] getNodes() {
+        return nodes;
+    }
+
+    public void setNodes(Vertix[] nodes) {
+        this.nodes = nodes;
+    }
+
+    public Vertix[] getReverseNodes() {
+        return reverseNodes;
+    }
+
+    public void setReverseNodes(Vertix[] reverseNodes) {
+        this.reverseNodes = reverseNodes;
+    }
+
+    public int[] getNodeIndex() {
+        return nodeIndex;
+    }
+
+    public void setNodeIndex(int[] nodeIndex) {
+        this.nodeIndex = nodeIndex;
+    }
+}
+
+class Vertix{
+
+    /**
+     * 值
+     * */
+    private char value;
+
+    /**
+     * 头结点
+     * */
+    private Node head;
+
+    public Vertix(char value, Node head) {
+        this.value = value;
+        this.head = head;
+    }
+
+    public char getValue() {
+        return value;
+    }
+
+    public void setValue(char value) {
+        this.value = value;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public void setHead(Node head) {
+        this.head = head;
+    }
+}
+
+
+/**
+ * GraphLinked
+ * @description 单链表
+ * @author caizhichong
+ * @date 2020/11/3
+ * @version V1.0
+ */
+class Node{
+
+    private char value;
+
+    private Node next;
+
+    public Node(char value, Node next) {
+        this.value = value;
+        this.next = next;
+    }
+
+    public char getValue() {
+        return value;
+    }
+
+    public void setValue(char value) {
+        this.value = value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
--- project/src/datastructure/graph/GraphMatrix.java
+++ project/src/datastructure/graph/GraphMatrix.java
@@ -1,5 +1,9 @@
 package datastructure.graph;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /**
  * GraphMatrix 图-邻接矩阵
  *
@@ -8,4 +12,218 @@ package datastructure.graph;
  * @date 2020年07月30日 8:53
  */
 public class GraphMatrix {
+
+    public static void main(String[] args) {
+        Graph graph = buildGraph();
+        List<Integer> noInDegreeVertiexs = graph.getStartVertixIndex();
+        boolean[] visited = new boolean[graph.getVertix().length];
+        for(Integer vertix : noInDegreeVertiexs){
+            //深度优先遍历
+            dfs(graph, vertix, visited);
+            //广度优先遍历
+            //bfs(graph, vertix, visited);
+        }
+    }
+
+
+    /**
+     * GraphMatrix
+     * @description 构建图
+     * @param
+     * @return
+     * @author caizhichong
+     * @date 2020/10/30
+     * @version V1.0
+     */
+    private static Graph buildGraph(){
+        /*char[] vertix = new char[]{'a','g','c','f','e','d','b'};
+        Graph graph = new Graph(vertix);
+        graph.addEdge('a', 'b');
+        graph.addEdge('a', 'c');
+        graph.addEdge('b', 'c');
+        graph.addEdge('b', 'd');
+        graph.addEdge('c', 'd');
+        graph.addEdge('c', 'g');
+        graph.addEdge('d', 'e');
+        graph.addEdge('d', 'f');
+        graph.addEdge('e', 'f');
+        graph.addEdge('g', 'f');*/
+        char[] vertix = new char[]{'a','g','c','f','e','d','b','i','h','j'};
+        Graph graph = new Graph(vertix);
+        graph.addEdge('a', 'b');
+        graph.addEdge('a', 'c');
+        graph.addEdge('b', 'g');
+        graph.addEdge('g', 'f');
+        graph.addEdge('g', 'h');
+        graph.addEdge('g', 'i');
+        graph.addEdge('d', 'c');
+        graph.addEdge('d', 'e');
+        graph.addEdge('e', 'i');
+        graph.addEdge('c', 'g');
+        graph.addEdge('b', 'f');
+        return graph;
+    }
+
+
+    /**
+     * GraphMatrix
+     * @description 深度优先遍历
+     * @param graph 图结构
+     * @param vertixIndex 当前遍历顶点下标
+     * @param visit 访问过的节点
+     * @return
+     * @author caizhichong
+     * @date 2020/7/31
+     * @version V1.0
+     */
+    private static void dfs(Graph graph, int vertixIndex, boolean[] visit){
+        if(visit[vertixIndex]){
+            return;
+        }
+        visit[vertixIndex] = true;
+        System.out.println("节点:" + graph.getVertix()[vertixIndex]);
+        for(int i = 0; i < graph.getMatrix()[vertixIndex].length; i++){
+            if(graph.getMatrix()[vertixIndex][i] == 1){
+                dfs(graph, i, visit);
+            }
+        }
+    }
+
+
+    /**
+     * GraphMatrix
+     * @description 广度优先遍历
+     * @param graph 图结构
+     * @param visit 访问过的节点
+     * @return
+     * @author caizhichong
+     * @date 2020/7/31
+     * @version V1.0
+     */
+    private static void bfs(Graph graph, int vertixIndex, boolean[] visit) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(vertixIndex);
+        Integer node;
+        while((node = queue.poll()) != null){
+            if(visit[node]){
+                continue;
+            }
+            System.out.println("节点：" + graph.getVertix()[node]);
+            visit[node] = true;
+            for(int i = 0; i < graph.getMatrix()[node].length; i++){
+                if(graph.getMatrix()[node][i] == 1){
+                    queue.add(i);
+                }
+            }
+        }
+    }
+
+}
+
+
+/**
+ * GraphMatrix
+ * @description 图数据结构
+ * @author caizhichong
+ * @date 2020/10/30
+ * @version V1.0
+ */
+class Graph{
+
+    /**
+     * 顶点
+     * */
+    private char [] vertix;
+
+    /**
+     * 用户记录每个顶点的下标位置（这里假设最多只有英文字母26个顶点）
+     * */
+    private int[] charIndex = new int[26];
+
+    /**
+     * 邻接矩阵
+     * */
+    private int[][] matrix;
+
+    public Graph(char [] vertix){
+        this.vertix = vertix;
+        this.init();
+    }
+
+    private void init(){
+        for(int i = 0; i < this.getVertix().length; i++){
+            this.charIndex[this.hashIndex(this.getVertix()[i])] = i;
+        }
+        matrix = new int[vertix.length][vertix.length];
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[i].length; j++){
+                matrix[i][j] = -1;
+            }
+        }
+    }
+
+    /**
+     * 添加边
+     * */
+    public void addEdge(char from, char to){
+        int fromIndex = this.getCharIndex()[this.hashIndex(from)];
+        int toIndex = this.getCharIndex()[this.hashIndex(to)];
+        this.matrix[fromIndex][toIndex] = 1;
+    }
+
+
+    /**
+     * Graph
+     * 简写哈希定位顶点下标
+     * @version V1.0
+     */
+    private int hashIndex(char vertix){
+        return vertix - 'a';
+    }
+
+    public char[] getVertix() {
+        return vertix;
+    }
+
+    public void setVertix(char[] vertix) {
+        this.vertix = vertix;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(int[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    public int[] getCharIndex() {
+        return charIndex;
+    }
+
+    public void setCharIndex(int[] charIndex) {
+        this.charIndex = charIndex;
+    }
+
+    /**
+     * Graph
+     * @description  获取任意一个没有入度的节点作为起点(获取对应顶点下标)
+     * @return
+     * @author caizhichong
+     * @date 2020/11/3
+     * @version V1.0
+     */
+    public List<Integer> getStartVertixIndex(){
+        List<Integer> noInDegreeVertix = new LinkedList<>();
+        outer:for(int i = 0; i < this.getMatrix().length;i++){
+            inner:for(int j = 0; j < this.getMatrix()[i].length; j++){
+                if(this.getMatrix()[j][i] == 1){
+                    continue outer;
+                }
+            }
+            noInDegreeVertix.add(i);
+        }
+        return noInDegreeVertix;
+    }
+
 }
